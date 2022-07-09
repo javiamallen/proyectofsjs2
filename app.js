bacdcfce56887f8175717dba6d7fa97203d5aa91
Javiera <javierafulls@gmail.com>
2022-07-09 00:02:28 -0400

    main.hbs

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
 
 const app = express();
+
+app.engine(
+    "hbs",
+    exphbs.engine({
+        extname: ".hbs",
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials'
+    })
+);
+
+app.set("view engine, hbs");
 
 app.listen(3000, () => console.log("servidor en http://localhost:3000"));
 
@@ -10,3 +22,5 @@ app.listen(3000, () => console.log("servidor en http://localhost:3000"));
 
 app.use('/', require('./src/routes/views'));
 app.use('/api', require('./src/routes/api'));
+
+
