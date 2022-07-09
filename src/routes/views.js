17e3ad3daf8387077d9112ba30371ec14b0f58e0
Javiera <javierafulls@gmail.com>
2022-07-09 16:34:25 -0400

    vistas todas OK

--- src/routes/views.js
+++ src/routes/views.js
@@ -14,4 +14,32 @@ router.get("/login", (req, res) => {
     res.render("Login");
 });
 
+// vista Registrarse 
+
+router.get("/register", (req, res) => {
+    res.render("Register");
+});
+
+// vista pedido
+
+router.get("/order", (req, res) => {
+    res.render("Order");
+});
+
+// vista producto indiv
+
+router.get("/product", (req, res) => {
+    res.render("Product");
+});
+
+// vista Lista de Productos
+
+router.get("/productList", (req, res) => {
+    res.render("productList");
+});
+router.get("/complete", (req, res) => {
+    res.render("complete");
+});
+
+
 module.exports = router;
